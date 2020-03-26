ccce0b6b51cf378f2a5d64e759e47bdf2c7e4553
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2020-03-26 22:48:10 +0100

    2441

---
+++ 2441.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N; 
+
+	cin >> N;
+
+	for (int i = N; i > 0; i--) {
+		for (int k = 0; k < (N - i); k++) {
+			cout << " ";
+		}
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}

720c18ace8c488509535d39c0e1ae51e49bf66ea
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2020-09-16 20:41:45 +0200

    Create 연구소.cpp
    재귀를 이용해 맵에서 무작위로 위치 추출하는 방법을 사용함. 재귀에서 map에 벽이 세워진위치가 바뀌며 cnt가 3이 될때까지 스택이 쌓이다가, bfs를 돌고 스택이 빠져서 다시 map이 원상복귀 되는 형식.
    dfs의 중단점이 cnt==3이므로, 요때 bfs를 돌도록 함수간을 엮는다.
    bfs를 돌떄 map에 대해 for문을 돌 필요가 없는게, 큐에서 바이러스 위치를 빼면서 상하좌우 탐색을 못할때까지 계속하므로.
    main에서 맵 저장할 때 바이러스 위치를 큐에 저장해두지만, bfs에서 큐를 넣고 빼고하므로, bfs내에서 큐를 만들어 복사하고 map도 bfs 내에서 마킹할 때 쓰는 용도로 따로 또 만들어야 한다. 큐에서 뺀 뒤에 그자리를 2로 만들지만, 큐에 넣을때는 아직 마킹안하고 visited 로만 체크하는 방식을 썼지만 그냥 바로 2로 만들고, 0인것만 넣어줘도 된다.
    2개는 항상 바이러스이므로 62C3의 경우의 수로 벽의 위치를 뽑고, 2를 시작점으로 최대 62칸을 탐색해야하므로, 62C3 * 62 만큼의 경우의 수, 100만대라서 1초 내에 가능하다.

---
+++ samsung/연구소.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int map[8][8], n, m, ret = 0;
+const int dj[4] = { 0, 0, -1, 1 }; // j는 열(가로) i는 행(세로), 상하좌우
+const int di[4] = { -1, 1, 0, 0 };
+
+struct Loc {
+	int r, c;
+	Loc(int a, int b) {
+		r = a; c = b;
+	}
+};
+
+queue<Loc> virus;
+
+void bfs() {
+	queue<Loc> virus_tmp = virus; //for문 안돌아도, 큐는 이렇게 복사할 수 있다. 
+
+	//현재 선택된 벽들에 대해 방문 체크용. 매 bfs마다 필요해서 여기에 만듬. 전에 방문한적이 있다면 그 방은 선택안하도록. 
+	int visited[8][8] = { 0, }; //초기화 방식.
+	// 강의서는 바이러스 위치도 방문 체크하는데, 벽에대해서만 필요하잖아? 아래 if문도 그렇고
+
+	//원래의 맵은 훼손하지 않고 새로 만든 맵으로 쓰려고. (조합 dfs에서 map이 바뀜.)
+	int back_up[8][8];
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			back_up[i][j] = map[i][j];
+		}
+	}
+
+	while (!virus_tmp.empty()) {
+		Loc vi_loc = virus_tmp.front();
+		virus_tmp.pop();
+
+		int row = vi_loc.r;
+		int col = vi_loc.c;
+		back_up[row][col] = 2;
+
+		for (int i = 0; i < 4; ++i) {
+			int n_r = row + di[i];
+			int n_c = col + dj[i];
+			
+			if (n_r < 0 || n_r >= n || n_c < 0 || n_c >= m) continue;
+			
+			else {
+				if (back_up[n_r][n_c] == 0 && visited[n_r][n_c] == 0) {
+					// 여기선 큐에 넣기만 하니깐 2라고 아직 체크 안하지. 
+					visited[n_r][n_c] = 1;
+					virus_tmp.push(Loc(n_r, n_c));
+				}
+			}
+		}
+
+	}
+
+	int candi = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if(back_up[i][j] == 0) ++candi;
+		}
+	}
+
+	if (ret < candi) ret = candi;
+}
+
+void select_wall(int cnt, int si, int sj) {
+	if (cnt == 3) {
+		bfs();
+		return;
+	}
+	else {
+		for (int i = si; i < n; ++i) {
+			for (int j = sj; j < m; ++j) {
+				if (map[i][j] == 0) { // 벽을 세워야만 bfs 로 넘어가야하는데, 안세워도 다음 재귀로 넘어가게 짜놧엇음;
+					map[i][j] = 1;
+					select_wall(cnt + 1, i, j);
+					map[i][j] = 0;
+				}
+			}
+			sj = 0; // 행이 바뀌었는데 여전히 같은 열을 가리키고 있으면 다음 행의 첫열에서 시작안함.
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m;
+
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cin >> map[i][j];
+			if (map[i][j] == 2) {
+				virus.push(Loc(i, j)); //원래는 위치를 어떤 벡터에 저장해두고 꺼내서 큐에 넣엇는데, 바로 큐로 사용하면 된다.
+			}
+		}
+	}
+
+	select_wall(0, 0, 0); // count, si, sj
+
+	cout << ret;
+}

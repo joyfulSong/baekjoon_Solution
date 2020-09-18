8d2f40386cc5e0bbbbf16ed55650113157499fc6
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2020-09-18 22:35:17 +0200

    Create 연산자 끼워넣기.cpp
    문제 조건에서 최소는 -10억 최대는 10억이라고 했기에 그것보다 넘는 범위를 최대 최소 범위로 두면 안된다. (나는 21억까지로 습관처럼 뒀더니 10억보다 작은 숫자가 min으로 출력이 되어버렸다.)
    for문을 돌 때, 조합으로 구해야한다는 생각에 다음 레벨로 넘겨준 값부터 for문을 돌도록 했더니, 레벨 0의 연산자 종류가 바뀌지 못하더라.
    스택을 쌓아서 재귀를 돌 때 몇번째 for문까지 돌았는지도 함께 저장된다. 따라서 0레벨에서 i==0일때 끝난 스택으로 돌아오면, pop out되고 op[0]++로 채워진뒤, 레벨 0에 연산자가 존재하는 op[2](*)가 첫번째 연산자로서 들어갈 수 있게된다. 다음 레벨에서 다시 for문이 0부터 돌 때는 op[0](+)에 값이 채워져있는 상태기에 들어갈 수 있다.

---
+++ samsung/연산자 끼워넣기.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, minimum = 1000000000, maximum = -1000000000;
+int number[11], oper[4], ch[10]; // +, -, *, / 0123 숫자로 조합만들고 바꿔서 사용.
+
+int cal(int a, int b, int op) {
+	int sum = 0;
+	switch (op) {
+	case 0:
+		sum = a + b;
+		break;
+	case 1:
+		sum = a - b;
+		break;
+	case 2:
+		sum = a * b;
+		break;
+	case 3:
+		sum = a / b;
+		break;
+	}
+	return sum;
+}
+
+void dfs(int l, int c) {
+	if (l == n-1) {
+		// 레벨은 0부터 시작. 즉 n-2레벨이 마지막 연산자 쓸 때고, n-1은 더이상 쓸 연산자 없음
+		int sum = number[0];
+		for (int i = 0; i < n-1; ++i) {
+			sum = cal(sum, number[i+1], ch[i]);
+		}
+		if (sum > maximum) maximum = sum;
+		if (sum < minimum) minimum = sum;
+	}
+
+	else {
+		for (int i = 0; i < 4; ++i) {
+			if (oper[i] != 0) {
+				ch[l] = i; // ch는 초기화 할 필요가 없는게 레벨별로 넣기 때문.
+				oper[i] -= 1; //양수면 하나 썼으니 빼주는거고
+				if (oper[i] == 0) dfs(l + 1, c + 1);
+				else dfs(l + 1, c); // 현재 연산자의 인덱스에 머무르도록.
+				oper[i] += 1;
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> number[i];
+	}
+
+	for (int i = 0; i < 4; ++i) {
+		cin >> oper[i];
+	}
+
+	dfs(0, 0);
+
+	cout << maximum << '\n';
+	cout << minimum << '\n';
+
+
+	return 0;
+}

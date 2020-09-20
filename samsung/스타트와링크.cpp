0d42d2a367ead0d2b286a872cbdf861e6b19502e
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2020-09-20 18:29:05 +0200

    Create 스타트와링크.cpp
    dfs 내 for문에서 i=c로 시작하는 경우와 i=0으로 시작하는 경우의 차이는, 후자의 경우, 이전 인덱스를 또 사용해야할 경우이고, 전자의 경우는 인덱스의 순서가 뒤짚힐 경우가 없을 때이다. 예를들어 123에 대해 12 13 23으로 충분하면 되는 경우와, 21 31도 필요한 경우가 있겠다.
    minimum값을 무조건 int형의 최대값 21억으로 두면 오류가 나더라. 이 문제의 경우 최대의 차이는 0과 n*100(각 숫자의 최대치는 100) 이다. (n인 이유는 12 21이므로 전체 n의 숫자에 대해 n개의 더해야 할 경우 발생.) 따라서 n의 최대가 20이므로 2000이 최대 차이값이다.
    
    처음에 틀렸던 이유는 a팀의 배열을 만들 때 dfs로 넘겨주는 인덱스를 c+1로 두었는데, 레벨의 끝까지 차지 않으면 for문에서 i값이 바뀌기에 dfs로는 i+1을 넘겨줘야 하는 것이다. c+1을 넘겨주면 이전 레벨에서 3을 넣었는데, 레벨 끝까지 안찼는데 for문의 범위 끝에 이미 도달했다면 또 3을 넣기 때문이다.

---
+++ samsung/스타트와링크.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int s[21][21]; //0번인덱스 사용 x
+int a[11], b[11]; //ch는 숫자를 안쓴거 확인하기 위해서
+int n, minimum = 2000;
+
+void dfs(int l, int c) {
+	if (l == (n / 2) + 1) {
+		int ch[21] = { 0, };
+		for (int i = 1; i < (n / 2) + 1; ++i) {
+			ch[a[i]] = 1;
+		}
+
+		int j = 1;
+		for (int i = 1; i < n + 1; ++i) {
+			if (ch[i] == 0) {
+				b[j] = i;
+				++j;
+			}
+		}
+		int start = 0, link = 0;
+		for (int i = 1; i < (n / 2); ++i) {
+			int ai = a[i]; int bi = b[i];
+			for (int j = i + 1; j < (n / 2) + 1; ++j) {
+				int aj = a[j]; int bj = b[j];
+				start += s[ai][aj] + s[aj][ai];
+				link += s[bi][bj] + s[bj][bi];
+			}
+		}
+ 		if (abs(start - link) < minimum) 
+			minimum = abs(start - link);
+		//cout << abs(start - link) << minimum << '\n';
+	}
+	else {
+		for (int i = c; i < n + 1; ++i) {
+			a[l] = i;
+			dfs(l + 1, i + 1); // c+1을 넣엇었음. c는 dfs로 넘어온 값이고, 레벨이 안차면 i 포문이 돌아서 i값이바뀌니깐 i를 넘겨줘야하는거지. c넘겨주면 이전에 c 썼는데 또 c를 사용하게되지.
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+
+	for (int i = 1; i < n+1; ++i) {
+		for (int j = 1; j < n+1; ++j) {
+			cin >> s[i][j];
+		}
+	}
+
+
+	dfs(1,1);
+
+	cout << minimum;
+	return 0;
+}

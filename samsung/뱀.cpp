cfaf61b4f9076cf5d3767bf7fb219bf838f75203
joyfulSong <45616059+joyfulSong@users.noreply.github.com>
2023-04-07 22:39:53 +0900

    Update 뱀.cpp

--- samsung/뱀.cpp
+++ samsung/뱀.cpp
@@ -1,84 +1,134 @@
-// 시뮬레이션 문제
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<vector>
+#include<deque>
+ 
+#define endl "\n"
+#define MAX 100
 using namespace std;
-
-int map[101][101]; // 0행과 0열은 안쓰이므로.
-char trace[101][101]; // 뱀의 방향전환 기록
-char dir[101]; // 1초부터
-int dx[4] = { 0, 1, 0, -1 }; //행
-int dy[4] = { 1, 0, -1, 0 }; //열
-		   //(시계방향) 위 오른 아래 왼
-		   // 0, 1, 2, 3
-		   // d면 dx, dy에서 증가, l이면 감소
-
-struct snake {
-	int h_x, h_y, tail_x, tail_y, h_dir, t_dir;
-	snake(int a, int b, int c, int d, int e, int f) {
-		h_x = a; h_y = b; tail_x = c; tail_y = d; h_dir = e; t_dir = f;
-	}
-};
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	int n, k, a, b, l, x;
-	char c;
-
-	cin >> n >> k;
-
-	for (int i = 0; i < k; ++i) {
-		cin >> a >> b;
-		map[a][b] = 2; //사과
-	}
-	cin >> l;
-	for (int i = 0; i < l; ++i) {
-		cin >> x >> c;
-		dir[x] = c; // 방향 기록
-	}
-	snake s = snake(1, 1, 1, 1, 2, 2);
-
-	int time = 0;
-	while (1) {
-		++time;
-		// 시간이 증가하고 동시에, 방향전환이 주어지지 않았다면 머리는 같은 방향으로 이동
-		s.h_x += dx[s.h_dir]; s.h_y += dy[s.h_dir];
-
-		//이동 후, 머리가 맵을 벗어나거나 1로 된(자기자신)곳과 만나면 멈춘다.
-		if (s.h_x < 1 || s.h_x > n || s.h_y < 1 || s.h_y > n || map[s.h_x][s.h_y] == 1) break;
-
-		//map에 체크 (나중에 충돌 확인하려고)
-		map[s.h_x][s.h_y] = 1; 
-		
-		// 사과 있으면 꼬리 위치 그대로. (1체크만 하는것.)
-
-		if (map[s.h_x][s.h_y] == 0) {// 아무것도 없다면
-			map[s.tail_x][s.tail_y] = 0; // 꼬리 있던 칸 비워주기.
-			s.tail_x += dx[s.t_dir]; s.tail_y += dy[s.t_dir]; // 같은 방향에 대해서 꼬리도 한칸씩 이동
-			
-		    //꼬리 이동 후 머리가 회전했던 곳에 도달햇다면, 그 지점에서 똑같이 방향전환 <--즉, 시간에 따른 위치 기록을 해야함.
-			if (trace[s.tail_x][s.tail_y] == 'D') { 
-				s.t_dir = (s.t_dir + 1) % 4;
-			}
-			else if (trace[s.tail_x][s.tail_y] == 'L') {
-				s.t_dir = (s.t_dir - 1) % 4;
-			}
-		}
-		else break; // 1이라면 자신의 몸이 있는곳과 만난것. 
-
-		if (dir[time] == 'D') {
-			//우선가던데로 이동하고서, 다음 sec부터 방향이 바뀌는 것임
-			s.h_dir = (s.h_dir + 1) % 4;
-				//if (s.dir > 3) s.dir = 0; // 이 대신에 dir = (dir + 1) % 4 하면 자동으로 그렇게 됨. 
-			trace[s.h_x][s.h_y] = 'D';
-
-		}
-		else if (dir[time] == 'L') {
-			s.h_dir = (s.h_dir - 1) % 4;
-			//if (s.dir < 0) s.dir = 3;
-			trace[s.h_x][s.h_y] = 'L';
-		}
-	}
-
-	cout << time;
-	return 0;
+ 
+int N, K, L;
+int MAP[MAX][MAX];
+ 
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+ 
+vector<pair<int, char>> V;
+ 
+void Print()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << MAP[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void Input()
+{
+    cin >> N >> K;
+    for (int i = 0; i < K; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        x = x - 1;
+        y = y - 1;
+        MAP[x][y] = 1;    // 사과 = 1로 표시
+    }
+    cin >> L;
+    for (int i = 0; i < L; i++)
+    {
+        int a; 
+        char b;
+        cin >> a >> b;
+        V.push_back(make_pair(a, b));
+    }
+}
+ 
+int Turn_Direction(int d, char c)
+{
+    if (c == 'L')
+    {
+        if (d == 0) return 3;
+        else if (d == 1) return 2;
+        else if (d == 2) return 0;
+        else if (d == 3) return 1;
+    }
+    else if (c == 'D')
+    {
+        if (d == 0) return 2;
+        else if (d == 1) return 3;
+        else if (d == 2) return 1;
+        else if (d == 3) return 0;
+    }
+}
+ 
+void Solution()
+{
+    deque<pair<int, int>> Q;
+    int x = 0;
+    int y = 0;
+    int d = 0;
+    int Time = 0;
+    int Idx = 0;
+    Q.push_back(make_pair(x, y));
+    MAP[x][y] = 2;
+ 
+    while (1)
+    {
+        Time++;
+ 
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        
+        if ((nx < 0 || ny < 0 || nx >= N || ny >= N) || MAP[nx][ny] == 2)
+        {
+            break;
+        }
+        else if (MAP[nx][ny] == 0)
+        {
+            MAP[nx][ny] = 2;
+            MAP[Q.back().first][Q.back().second] = 0;
+            Q.pop_back();
+            Q.push_front(make_pair(nx, ny));
+        }
+        else if (MAP[nx][ny] == 1)
+        {
+            MAP[nx][ny] = 2;
+            Q.push_front(make_pair(nx, ny));
+        }
+        
+ 
+        if (Idx < V.size())
+        {
+            if (Time == V[Idx].first)
+            {
+                if (V[Idx].second == 'L') d = Turn_Direction(d, 'L');
+                else if (V[Idx].second == 'D') d = Turn_Direction(d, 'D');
+                Idx++;
+            }
+        }
+        x = nx;
+        y = ny;
+    }
+    cout << Time << endl;
+}
+ 
+void Solve()
+{
+    Input();
+    Solution();
+}
+ 
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    //freopen("Input.txt", "r", stdin);
+    Solve();
+ 
+    return 0;
 }
